0b91811d788c9cab06c3520fb92723161c98ed00
Joe Klause <joeklause@gmail.com>
2019-09-17 09:28:16 -0700

    got a test to pass!

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,10 +1,11 @@
-const input = [1, 2, 3, 4, 5, 6];
-
+/* eslint-disable no-unused-vars */
 function reverseArray(array){
   const length = array.length - 1;
   const resultArray = [];
   for(let i = length; i >= 0; i--) {
-    resultArray.push(array[i])
+    resultArray[resultArray.length] = (array[i]);
   }
   return resultArray;
 }
+
+module.exports = reverseArray;
